dd74bef36acccee349caff47fe5729bd455c8c3f
darshantalati23 <darshantalati23@gmail.com>
2025-04-11 17:11:57 +0530

    preorder traversal recursion

---
+++ Binary Trees/preTrav.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fr(T, s, e, it) for(T i=s; i<e; i+=it)
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int n) : data(n), left(nullptr), right(nullptr) {}
+};
+
+/*
+ENTER ROOT, LEFT AND RIGHT IN RESPECTIVE ORDER. '-1' TO SKIP THAT CHILD/ROOT.
+*/
+TreeNode* buildTree() {
+    // cout << "Enter root value (or -1 for NULL): ";
+    int val;
+    cin >> val;
+    if (val == -1) return nullptr;
+
+    TreeNode* root = new TreeNode(val);
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        // cout << "Enter left child of " << node->data << " (or -1 for NULL): ";
+        int leftVal;
+        cin >> leftVal;
+        if (leftVal != -1) {
+            node->left = new TreeNode(leftVal);
+            q.push(node->left);
+        }
+
+        // cout << "Enter right child of " << node->data << " (or -1 for NULL): ";
+        int rightVal;
+        cin >> rightVal;
+        if (rightVal != -1) {
+            node->right = new TreeNode(rightVal);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+void preOrder(TreeNode* root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+signed main() {
+    TreeNode* root = buildTree();
+    preOrder(root);
+    return 0;
+}
